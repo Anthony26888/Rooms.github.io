9e9cb6161749baca7c3bcbc30aed9f885878b094
Anthoy Nguyen <haidang11111995@gmail.com>
2024-03-14 00:16:46 +0700

    update

---
+++ src/components/Card/Service.vue
@@ -0,0 +1,61 @@
+<template lang="">
+  <div class="d-flex  justify-item-start">
+    <v-sheet
+      class="pa-4 text-center mx-auto"
+      elevation="5"
+      width="150"
+      height="120"
+      rounded="lg"
+    >
+      <v-icon
+        class="mb-3"
+        color="orange"
+        icon="mdi-lightning-bolt"
+        size="50"
+      ></v-icon>
+      <h4 class="b-6">ĐIỆN</h4>
+    </v-sheet>
+
+    <v-sheet
+      class="pa-4 text-center mx-auto"
+      elevation="5"
+      width="150"
+      height="120"
+      rounded="lg"
+    >
+      <v-icon
+        class="mb-3"
+        color="primary"
+        icon="mdi-water"
+        size="50"
+      ></v-icon>
+      <h4 class="mb-6">NƯỚC</h4>
+    </v-sheet>
+
+    <v-sheet
+      class="pa-4 text-center mx-auto"
+      elevation="5"
+      width="150"
+      height="120"
+      rounded="lg"
+    >
+      <v-icon
+        class="mb-3"
+        color="primary"
+        icon="mdi-lightning-bolt"
+        size="50"
+      ></v-icon>
+      <h4 class="mb-6">DỊCH VỤ THÊM</h4>
+    </v-sheet>
+  </div>
+</template>
+<script>
+export default {
+    data() {
+        return {
+           
+        }
+    },
+};
+</script>
+<style lang=""></style>
--- src/views/Service.vue
+++ src/views/Service.vue
@@ -1,10 +1,10 @@
 <template>
     <vContainer>
-        
+        <Card/>
     </vContainer>
 </template>
     
 <script setup>
-import List from '@/components/Card/Rooms.vue'
-import Add from '@/components/Card/AddRoom.vue'
+import Card from '@/components/Card/Service.vue'
+
 </script>
