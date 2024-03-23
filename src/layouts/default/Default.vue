2c9120aec5816277060ed982b183244f0bb13c12
Anthony26888 <haidang11111995@gmail.com>
2024-03-23 10:45:27 +0700

    update

--- src/layouts/default/Default.vue
+++ src/layouts/default/Default.vue
@@ -1,19 +1,19 @@
 <template>
   <v-app class="bg-grey-lighten-4">  
     <div class="d-none d-lg-flex d-xl-none">
-      <DefaultView/> 
-      <DefaultNavigation/>
+      <default-view class="w-100"></default-view> 
+      <default-navigation></default-navigation>
     </div>
     <div class="d-lg-none d-xl-flex">
-      <DefaultView/> 
-      <DefaultBottom/>
+      <default-view class="w-100"></default-view>
+      <default-bottom></default-bottom>
     </div>
     
   </v-app>
 </template>
 
 <script setup>
-  import DefaultBar from './AppBar.vue'
+  
   import DefaultView from './View.vue'
   import DefaultNavigation from './Navigation.vue'
   import DefaultBottom from './BottomNavigation.vue'
