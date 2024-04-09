b47d1718d25c032d742e25369651dfc2940d8752
Anthony26888 <107421585+Anthony26888@users.noreply.github.com>
2024-04-10 00:12:46 +0700

    Update app.js

--- src/store/app.js
+++ src/store/app.js
@@ -6,8 +6,8 @@ import { useLocalStorage } from "@vueuse/core";
 export const useAppStore = defineStore("app", {
   state: () => {
     return {
-      RestApi: "https://n7hnx2-3000.csb.app",
-      Url: "http://localhost:3000",
+      Url: "https://n7hnx2-3000.csb.app",
+      Urlapi: "http://localhost:3000",
       NumberRoom: "",
       IdRoom: useLocalStorage("IdRoom", ""),
       IdMember: "",
