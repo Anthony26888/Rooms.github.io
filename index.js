69c7dc86f8f8ed9244b23e3c935f13d9be2529be
Anthony26888 <haidang11111995@gmail.com>
2024-04-12 14:22:34 +0700

    update

--- index.js
+++ index.js
@@ -1,8 +1,22 @@
-const jsonServer = require('json-server');
+const jsonServer = require("json-server");
 const server = jsonServer.create();
-const router = jsonServer.router('/src/api/User.json');
+const router = jsonServer.router("/src/api/User.json");
 const middlewares = jsonServer.defaults();
 const port = process.env.PORT || 3000;
+
+// Add custom middleware for CORS
+server.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*"); // Allow any origin
+  res.header(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PUT, DELETE, PATCH, OPTIONS"
+  );
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
 
 server.use(middlewares);
 server.use(router);
